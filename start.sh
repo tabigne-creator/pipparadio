a18e404a9c698c95941eac4fa5ab0dd3ed073b97
tabigne-creator <tabigne@gmail.com>
2026-02-05 20:13:44 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 echo "=== AVVIO RADIO ==="
-
-# Avvia Icecast in background
 echo "1. Avvio Icecast..."
 icecast2 -c /etc/icecast2/icecast.xml &
-
-# Attendi che Icecast sia pronto
-echo "2. Attendo Icecast..."
 sleep 5
-
-# Avvia lo streamer Python dal percorso corretto
-echo "3. Avvio streamer Python..."
+echo "2. Avvio Streamer Python..."
 python3 /app/streamer.py
-
-echo "=== RADIO ATTIVA ==="
